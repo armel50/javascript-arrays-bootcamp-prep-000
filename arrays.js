769eadd5a7afe36f4e5bbca2b04b569133e4bed3
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 02:24:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars= ["snickers","hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array,adding){
-  var newArray=array;
+  var newArray=[]array;
   newArray.unshift(adding);
   return newArray;
 }
