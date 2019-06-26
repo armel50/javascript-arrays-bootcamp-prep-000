bdb3b063545397be6a3a384031eea61c701d7c6c
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 02:43:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array;
 }
 function removeElementFromEndOfArray(array){
-  var slice = array.slice(-1);
+  var slice = array.slice(-2);
  return slice;
 }
 function destructivelyRemoveElementFromEndOfArray(array){
